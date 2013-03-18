c907a33c18b384967b872ee5cdde27fb0a021278
Carlos Tames <carlost82@gmail.com>
2013-03-18 12:28:16 -0600

    Creamos Status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pechibook::Application.config.secret_token = '09ddc8d28445c481184d459b7b9c3ff0da83f00ed8d4bc970ab336719dbfd8c03a702719e4bc18692e7c042b5396073d5816fbf1ee27f67c4d6f1c58c1ca2d92'
